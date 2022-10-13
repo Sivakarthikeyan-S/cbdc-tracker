aa687a1f5866bb619bd3c99747ab19201c4e8836
sivakarthi6055@gmail.com <sivakarthi6055@gmail.com>
2022-10-13 17:04:25 +0530

    update CBDC features

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 var series = [
-  ["USA",10,'Proof of concept'],
-  ["CAN",20,'Proof of concept'],
-  ["ZAF",30,'Proof of concept'],
-  ["PAK",40,'Proof of concept'],
-  ["CHE",50,'Proof of concept'],
-  ["NGA",50,'Proof of concept'],
-  ["GBR",60,'Proof of concept'],
-  ["IND",70,'Proof of concept'],
-  ["VNM",80,'Proof of concept'],
-  ["AUS",90,'Proof of concept'],
-  ["RUS",100,'Proof of concept']
+  ["PHL",20],
+  ["HTI",20],
+  ["DNK",20],
+  ["NLD",20],
+  ["BEL",20],
+  ["IRQ",20],
+  ["JOR",20],
+  ["CZE",20],
+  ["SVK",20],
+  ["AUT",20],
+  ["SVN",20],
+  ["ITA",20],
+  ["EST",20],
+  ["MEX",20],
+  ["FIN",20],
+  ["LVA",20],
+  ["LTU",20],
+  ["USA",20],
+  ["ZAF",20],
+  ["NAM",20],
+  ["MDG",20],
+  ["TZA",20],
+  ["ZWE",20],
+  ["PAK",20],
+  ["UGA",20],
+  ["KEN",20],
+  ["ZMB",20],
+  ["CHE",20],
+  ["GBR",20],
+  ["TUN",20],
+  ["MAR",20],
+  ["ESP",20],
+  ["PRT",20],
+  ["IND",20],
+  ["IRL",20],
+  ["VNM",20],
+  ["YEM",20],
+  ["OMN",20],
+  ["EGY",20],
+  ["ARG",20],
+  ["PER",20],
+  ["BGD",20],
+  ["ISL",20],
+  ["POL",20],
+  ["DEU",20],
+  ["NOR",20],
+  ["BGD",20],
+  ["THA",60],
+  ["HUN",60],
+  ["AUS",60],
+  ["SWE",60],
+  ["RUS",60],
+  ["JPN",60],
+  ["TUR",60],
+  ["IRN",60],
+  ["KAZ",60],
+  ["IDN",60],
+  ["MYS",60],
+  ["UKR",60],
+  ["NZL",60],
+  ["BRA",60],
+  ["KOR",60],
+  ["ISR",60],
+  ["NGA",100],
+  ["CAN",100],
+  ["SAU",100],
+  ["URY",100],
+  ["FRA",100],
+  ["GHA",100],
+  ["CHN",100]
 ];
-      
+
 var dataset = {};
 var time = Date.now();
 var autorotate = [39.666666666666664, -30];
@@ -19,7 +78,6 @@ var velocity = [.015, -0];
 // We need to colorize every country based on "partnerLink"
 // colors should be uniq for every value.
 // For this purpose we create palette(using min/max series-value)
-var proof = series.map(function(obj){ return obj[2]; });
 var onlyValues = series.map(function(obj){ return obj[1]; });
 var minValue = Math.min.apply(null, onlyValues);
 var maxValue = Math.max.apply(null, onlyValues);
@@ -63,7 +121,7 @@ function init() {
     projectionConfig: {
       rotation: globalRotation
     },
-    fills: {defaultFill: '#3a3a3a'},
+    fills: {defaultFill: '#1f1f1f'},
     data: dataset,
     geographyConfig: {
       responsive: true,
@@ -73,9 +131,9 @@ function init() {
       // don't change color on mouse hover
       highlightFillColor: function(geo) {
           if (typeof geo.partnerLink !== "undefined") {
-            return 'white';
+            return '#1177E1';
           }
-          return geo['fillColor'] || '#3a3a3a'
+          return geo['fillColor'] || '#1f1f1f'
           
       },
       // only change border
@@ -84,10 +142,24 @@ function init() {
         // don't show tooltip if country don't present in dataset
         if (!data) { return ; }
         // tooltip content
-        return ['',
+        if (data.partnerLink <= 20) {
+          return ['',
             '<div style="opacity:1;" class="hoverinfo">' + geo.properties.name,
-                '<br>' + data.partnerLink + ' hectares', '<br>' + proof,
+                '<br>' + 'Research',
                 ''].join('');        
+        }
+        if (data.partnerLink == 60) {
+          return ['',
+            '<div style="opacity:1;" class="hoverinfo">' + geo.properties.name,
+                '<br>' + 'Proof of concept',
+                ''].join('');        
+        }
+        if (data.partnerLink == 100) {
+          return ['',
+            '<div style="opacity:1;" class="hoverinfo">' + geo.properties.name,
+                '<br>' + 'Pilot',
+                ''].join('');        
+        }
       },
       done: function(datamap) {
         datamap.svg.call(d3.behavior.zoom().on("zoom", redraw));
